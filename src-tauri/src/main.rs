b5e46328fd4663900728f0a7b50b6008deb32839
ヽ◕◡◕✿ノ <stephenkamenar@gmail.com>
2026-09-15 07:17:08 -0400

    Launch Hyperact with global hotkey

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,7 +1,44 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::Manager;
+use tauri_plugin_global_shortcut::{Code, GlobalShortcutExt, Modifiers, Shortcut, ShortcutState};
+
 fn main() {
+    let launcher_shortcut = Shortcut::new(Some(Modifiers::ALT), Code::Space);
+
     tauri::Builder::default()
+        .plugin(
+            tauri_plugin_global_shortcut::Builder::new()
+                .with_handler(move |app, shortcut, event| {
+                    if shortcut != &launcher_shortcut || event.state() != ShortcutState::Pressed {
+                        return;
+                    }
+
+                    if let Some(window) = app.get_webview_window("main") {
+                        if window.is_visible().unwrap_or(false) {
+                            let _ = window.hide();
+                        } else {
+                            let _ = window.show();
+                            let _ = window.set_focus();
+                        }
+                    }
+                })
+                .build(),
+        )
+        .setup(move |app| {
+            app.global_shortcut().register(launcher_shortcut)?;
+
+            if let Some(window) = app.get_webview_window("main") {
+                let window_to_hide = window.clone();
+                window.on_window_event(move |event| {
+                    if matches!(event, tauri::WindowEvent::Focused(false)) {
+                        let _ = window_to_hide.hide();
+                    }
+                });
+            }
+
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running Hyperact");
 }
