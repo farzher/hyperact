6967cbe847f00274676e17bc3a5ba0ca8275d58c
ヽ◕◡◕✿ノ <stephenkamenar@gmail.com>
2026-09-16 00:22:45 -0400

    Register Everything file search commands

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,6 +3,7 @@
 use tauri::{AppHandle, Manager};
 use tauri_plugin_global_shortcut::{Code, Modifiers, ShortcutState};
 
+mod everything;
 #[cfg(target_os = "windows")]
 mod windows_input;
 #[cfg(target_os = "windows")]
@@ -490,7 +491,10 @@ fn main() {
             resume_hotkey_command,
             submit_prompt_result,
             modifiers_held,
-            non_ctrl_modifiers_held
+            non_ctrl_modifiers_held,
+            everything::search_files,
+            everything::open_path,
+            everything::reveal_path
         ])
         .run(tauri::generate_context!())
         .expect("error while running Hyperact");
